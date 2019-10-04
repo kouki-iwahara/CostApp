b0452ca38f7a098b1db7103c37b173e2f552d36d
岩原こうき <kouki.iwahara19860712@gmail.com>
2019-10-04 13:00:38 +0900

    headerの調整

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -2,19 +2,12 @@
   <div class="container-fluid">
     <div class="sidebar row">
       <div class="sidebar_search col-sm-3 hidden-xs  bg-dark">
-        <div class="input-group mb-3">
-          <input
-            type="text"
-            class="form-control"
-            placeholder="..."
-            aria-label="..."
-            aria-describedby="button-addon2"
-          />
-          <div class="input-group-append">
-            <button id="button-addon2" type="button" class="btn btn-info">
-              検索
-            </button>
-          </div>
+        <div class="nav-list_info bg-info text-dark">
+          <p>
+            <slot>
+              全ての食材
+            </slot>
+          </p>
         </div>
       </div>
       <div class="sidebar_list col-sm-3 hidden-xs bg-dark">
@@ -64,6 +57,16 @@ export default {
   -webkit-filter: brightness(0.9);
   filter: brightness(0.9);
 }
+.nav-list_info {
+  font-size: 20px;
+  font-weight: bold;
+  /* background-color: #f4f5f7; */
+  text-align: center;
+  line-height: 50px;
+  margin: 0px 0px;
+  height: 50px;
+  border-radius: 5px 5px 0 0;
+}
 
 @media screen and (min-width: 768px) {
   .sidebar_search {
@@ -74,7 +77,7 @@ export default {
   }
   .sidebar_list {
     position: absolute;
-    top: 124px;
+    top: 120px;
     bottom: 0;
     left: 0;
     display: block;
