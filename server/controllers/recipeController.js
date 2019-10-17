2d80a137541ca80aab3a4278f7da8ce50fec96bc
岩原こうき <kouki.iwahara19860712@gmail.com>
2019-10-17 09:52:08 +0900

    レスポンスの送り方をjsonに修正

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -139,7 +139,7 @@ const recipeController = {
     // 食材を選んでいなかったらこのままフロントへ返す
     if (!req.body.foods) {
       // 成功でレシピデータを返す
-      res.status(200).send({ message: '登録完了です', result: updatedRecipe })
+      res.status(200).json({ message: '更新しました', result: updatedRecipe })
       return
     }
     // レシピ食材更新処理
@@ -219,14 +219,14 @@ const recipeController = {
           })
           .catch((error) => {
             console.log(error)
-            res.status(404).send({ error: error.message })
+            res.status(404).json({ error: error.message })
           })
         // 削除処理
         const deletedFoodRecipe = await food
           .destroy({ force: true })
           .catch((error) => {
             console.log(error)
-            res.status(404).send({ error: error.message })
+            res.status(404).json({ error: error.message })
           })
         console.log(deletedFoodRecipe)
       }
@@ -234,7 +234,7 @@ const recipeController = {
     console.log(updatedFoodRecipes)
     // 更新されたレシピに食材データを格納しフロントに返す
     updatedRecipe.dataValues.foods = updatedFoodRecipes
-    res.status(200).send({ message: '更新しました', result: updatedRecipe })
+    res.status(200).json({ message: '更新しました', result: updatedRecipe })
   }
 }
 
